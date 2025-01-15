600014a9a7cb5f876e23313b6fa3113fddbab71a
Saibharath1253 <saibharath1253@gmail.com>
2025-01-15 21:19:03 +0530

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,13 +1,13 @@
 class Publisher:
-    def _init_(self, name):
+    def __init__(self, name):
         self.name = name
 
     def display_info(self):
         print(f"Publisher: {self.name}")
 
 class Book(Publisher):
-    def _init_(self, name, title, author):
-        super()._init_(name)
+    def __init__(self, name, title, author):
+        super().__init__(name)
         self.title = title
         self.author = author
 
@@ -17,7 +17,22 @@ class Book(Publisher):
         print(f"Author: {self.author}")
 
 class Python(Book):
-    def _init_(self, name, title, author, price, pages):
-        super()._init_(name, title, author)
+    def __init__(self, name, title, author, price, pages):
+        super().__init__(name, title, author)
         self.price = price
-        self.pages = pages
+        self.pages = pages
+
+    def display_info(self):
+        super().display_info()
+        print(f"Price: {self.price}")
+        print(f"Pages: {self.pages}")
+
+
+publisher_name = input("Enter publisher name: ")
+book_title = input("Enter book title: ")
+book_author = input("Enter book author: ")
+price = float(input("Enter book price: "))
+pages = int(input("Enter number of pages: "))
+
+python_book = Python(publisher_name, book_title, book_author, price, pages)
+python_book.display_info()
